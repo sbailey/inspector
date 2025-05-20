ae9bd929f5593a12db9841e28b69cee63a0df19e
Stephen Bailey <stephen.code@gmail.com>
2025-05-20 16:27:17 -0700

    WIP flask vs other refactor

--- app.py
+++ app.py
@@ -40,41 +40,6 @@ _MAX_RADIUS_ERROR_MESSAGE = f'Please limit your search to radius < {_MAX_RADIUS}
 _MAX_SPECTRA=1000
 _MAX_SPECTRA_ERROR_MESSAGE = '{} spectra is more than we can realistically display; please limit your search to fewer than {} spectra'
 
-def _standardize_specprod(specprod):
-    """
-    Return standardized specprod with aliases for dr1 -> iron, etc.
-    """
-    specprod = specprod.lower()
-    if specprod == 'edr':
-        return 'fuji'
-    elif specprod == 'dr1':
-        return 'iron'
-    elif specprod == 'dr2':
-        return 'loa'
-    else:
-        return specprod
-
-def _parse_fibers(fibers_string):
-    """
-    Parse fiber strings like "0-5,10:12,25" -> [0,1,2,3,4,5,10,11,25]
-    Note: A-B is inclusive of B, while A:B is python-like exclusive of B
-    """
-    fibers = list()
-    for token in fibers_string.split(','):
-        if token.isdigit():
-            fibers.append(int(token))
-        elif '-' in token:
-            first, last = token.split('-')
-            fibers.extend( np.arange(int(first), int(last)+1) )
-        elif ':' in token:
-            first, last = token.split(':')
-            fibers.extend( np.arange(int(first), int(last)) )
-        else:
-            raise ValueError(f'Failed to parse {token} as part of {fibers_string}')
-
-    return fibers
-
-
 @app.route("/testargs")
 def test_filter():
     print(type(request.args))
@@ -267,97 +232,9 @@ def get_filters():
     """
     return request.args.to_dict(flat=False)
 
-def _filter_table(table, args=None):
-    """
-    Filter a Table based upon URL args dict
-
-    TODO: expand explanation, args comes from request.args.to_dict(flat=False)
-    """
-    if args is None:
-        args = request.args.to_dict(flat=False)
-    elif isinstance(args, werkzeug.datastructures.structures.MultiDict):
-        args = args.to_dict(flat=False)
-
-    keep = np.ones(len(table), dtype=bool)
-    for column, filter_list in args.items():
-        if column not in table.colnames:
-            continue
-        for filt in filter_list:
-            if ':' in filt:
-                operator, value = filt.split(':')
-            else:
-                operator = 'eq'
-                value = filt
-
-            value = np.array(value, dtype=table[column].dtype)
-            print(f'Filter {column} {operator} {value}')
-
-            if operator == 'eq':
-                keep &= (table[column] == value)
-            elif operator == 'lt':
-                keep &= (table[column] < value)
-            elif operator == 'le':
-                keep &= (table[column] <= value)
-            elif operator == 'gt':
-                keep &= (table[column] > value)
-            elif operator == 'ge':
-                keep &= (table[column] >= value)
-            elif operator == 'ne':
-                keep &= (table[column] != value)
-            else:
-                #- TODO: handle error message with error message to user
-                print(f'ERROR: unrecognized operator {operator}')
-
-    return table[keep]
-
-def _add_zcat_columns(targetcat, specprod):
-    """
-    Return copy of targetcat with zcat columns added
-
-    Adds TARGET_RA, TARGET_DEC, SPECTYPE, Z, ZWARN
-    """
-    t = targetcat.copy(copy_data=False)
-    zcat = read_redrock_targetcat(t, fmcols=['TARGET_RA', 'TARGET_DEC'], specprod=specprod)
-    for col in ['TARGET_RA', 'TARGET_DEC', 'SPECTYPE', 'Z', 'ZWARN']:
-        t[col] = zcat[col]
-
-    return t
-
-def _parse_radec_string(radec):
-    try:
-        ra,dec,radius = inventory.parse_radec(radec)
-    except ValueError as err:
-        message = f'Could not parse "{radec}" as RA,DEC,RADIUS: {str(err)}'
-        raise ValueError(message)
-
-    if radius > MAX_RADIUS:
-        raise ValueError(MAX_RADIUS_ERROR_MESSAGE)
-
-    return ra,dec,radius
 
 #-------------------------------------------------------------------------
 #- Inventory of targets
-
-def _load_targets(specprod, specgroup, radec=None, targetids=None):
-    """
-    required: specprod, specgroup; plus radec OR targetids (but not both)
-    """
-    specprod = standardize_specprod(specprod)
-
-    if radec is not None:
-        radec = validate_radec(radec)
-    elif targetids is not None:
-        targetids = list(map(int, targetids.split(',')))
-    else:
-        raise ValueError('must specify radec or targetids')
-
-    if specgroup == 'healpix':
-        t = inventory.target_healpix(radec=radec, targetids=targetids, specprod=specprod)
-    elif specgroup == 'tiles':
-        t = inventory.target_tiles(radec=radec, targetids=targetids, specprod=specprod)
-
-    t = filter_table(add_zcat_columns(t, specprod))
-    return t
 
 def render_targets(specprod, specgroup, radec=None, targetids=None):
     """
@@ -510,25 +387,6 @@ def render_spectra_fits(spectra):
     response.headers['Content-Type'] = 'application/fits'
 
     return response
-
-def _load_spectra(specprod, specgroup, radec=None, targetids=None, maxspectra=MAX_SPECTRA):
-    """
-    Required: specprod, specgroup; and radec OR targetids (but not both)
-
-    TODO: separate loading spectra from flask-specific rendering
-    """
-    specprod = standardize_specprod(specprod)
-    targetcat = load_targets(specprod, specgroup=specgroup, radec=radec, targetids=targetids)
-
-    num_spectra = len(targetcat)
-    if num_spectra == 0:
-        return None
-    elif num_spectra > MAX_SPECTRA:
-        raise ValueError(MAX_SPECTRA_ERROR_MESSAGE.format(num_spectra, maxspectra))
-
-    print(f'Reading {num_spectra} spectra')
-    spectra = read_spectra_parallel(targetcat, specprod=specprod, rdspec_kwargs=dict(return_redshifts=True))
-    return spectra
 
 def render_spectra(specprod, specgroup, radec=None, targetids=None):
     try:
